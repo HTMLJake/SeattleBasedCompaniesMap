b2a227f700c4519984dc599c7cb3581c67d87b4d
Jacob Perry <btjake@TheMetalFruit.home>
2018-10-16 11:59:54 -0700

    Updates

---
+++ precache-manifest.192d9d1b8103d21127562432fb7db3aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "967c0668458ea644b584",
+    "url": "/SeattleBasedCompaniesMap/static/js/runtime~main.967c0668.js"
+  },
+  {
+    "revision": "69d434c70e508d67d725",
+    "url": "/SeattleBasedCompaniesMap/static/js/main.69d434c7.chunk.js"
+  },
+  {
+    "revision": "ef6b9113e55c1f903f7c",
+    "url": "/SeattleBasedCompaniesMap/static/js/1.ef6b9113.chunk.js"
+  },
+  {
+    "revision": "69d434c70e508d67d725",
+    "url": "/SeattleBasedCompaniesMap/static/css/main.9bfdf7b9.chunk.css"
+  },
+  {
+    "revision": "deedd89e0e42604a9c4f3a294971d151",
+    "url": "/SeattleBasedCompaniesMap/index.html"
+  }
+];
